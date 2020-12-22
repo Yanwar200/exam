05e8901dbe97e238e57f4bd5761cb7f8c7feb784
Yanwar200 <golubencevy01@mail.ru>
2020-12-22 12:34:49 +0300

    exam

---
+++ ex5.cpp
@@ -0,0 +1,35 @@
+﻿
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    ifstream f ("primer.txt");
+    int stringCount = 0;
+    while (f)
+    {
+        string s;
+        getline(f, s);
+        int wordCount = 1;
+        int simbolCount = s.length();
+        if (simbolCount > 0)
+        {
+            stringCount++;
+            cout << simbolCount << "  " << wordCount << endl;
+            continue;
+        }
+        for (int i = 0; i < simbolCount; i++)
+        {
+            if (s[i] == ' ')
+            {
+                wordCount++;
+            }
+        }
+        
+    }
+    cout << stringCount << endl;
+}
+
